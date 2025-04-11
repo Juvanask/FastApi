63b88f6c7b52bdafac95fe4c0c7ec60fd77318f8
Juvanask <sayeedajuvanakhan@gmail.com>
2025-04-11 20:15:10 +0530

    backend

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routers.auth import router as auth_router
 from routers.coin import router as coin_router
@@ -8,3 +9,6 @@ app = FastAPI(title="Authentication + Binance + Weather API")
 app.include_router(auth_router, prefix="/auth")
 app.include_router(coin_router, prefix="/coins")
 app.include_router(weather_router, prefix="/weather")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=10000, reload=True)
